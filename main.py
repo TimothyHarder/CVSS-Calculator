59fa29ac5103f09b2bf65b86947b0634179799ff
Timothy Harder <harder.timothy.j@gmail.com>
2020-12-07 18:43:26 +0000

    Finished Environmental and Temporal calculators, fixed roundup function problem, changed how the base score was calculated to match how the others were for easily updating the object.

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-
-# 'PR' (Privileges Required) values L and H get modified if the 'S' (scope) is changed.
-# if 'S' == 'C', PR: L = 0.68 and PR: H = 0.5
-
-
-
 class CVSS():
   def __init__(self, metrics, base_score=None):
     """
@@ -12,43 +6,50 @@ class CVSS():
       base_score (double): Number representing the base cvss score. This is often provided by the software/hardware developer issuing the security notice. Optional.
     """
 
+    self.e = "x"
+    self.rl = "x"
+    self.rc = "x"
+    self.cr = "x"
+    self.ir = "x"
+    self.ar = "x"
+    self.mav = "x"
+    self.mac ="x"
+    self.mpr = "x"
+    self.mui = "x"
+    self.ms = "x"
+    self.mc = "x"
+    self.mi = "x"
+    self.ma = "x"
+
+    self.vs = ''
+
     if base_score:
       self.base_score = base_score
+      # TODO: Set up the metrics for this.
     else:
-      metrics = {k.lower(): v for k, v in metrics.items()}
-      self.metrics = metrics
-      print(self.metrics)
-      try:
-        self.av = self.metrics['av'].lower()
-        self.ac = self.metrics['ac'].lower()
-        self.pr = self.metrics['pr'].lower()
-        self.ui = self.metrics['ui'].lower()
-        self.s = self.metrics['s'].lower()
-        self.c = self.metrics['c'].lower()
-        self.i = self.metrics['i'].lower()
-        self.a = self.metrics['a'].lower()
-        self.e = "x"
-        self.rl = "x"
-        self.rc = "x"
-        self.cr = "x"
-        self.ir = "x"
-        self.ar = "x"
-        self.mav = "x"
-        self.mac ="x"
-        self.mpr = "x"
-        self.mui = "x"
-        self.ms = "x"
-        self.mc = "x"
-        self.mi = "x"
-        self.ma = "x"
+      self.set_base_metrics(metrics)
 
-      except KeyError():
-        raise KeyError('Not all mandatory metrics were set.')
-      
-      self.base_score = self.__calculate_base_score()
-      self.temporal_score = None
-      self.environmental_score = None
-      
+
+  def set_base_metrics(self, base_metrics):
+    metrics = {k.lower(): v for k, v in base_metrics.items()}
+    self.metrics = metrics
+    try:
+      self.av = self.metrics['av'].lower()
+      self.ac = self.metrics['ac'].lower()
+      self.pr = self.metrics['pr'].lower()
+      self.ui = self.metrics['ui'].lower()
+      self.s = self.metrics['s'].lower()
+      self.c = self.metrics['c'].lower()
+      self.i = self.metrics['i'].lower()
+      self.a = self.metrics['a'].lower()
+    except KeyError():
+      raise KeyError('Not all mandatory metrics were set.')
+
+    self.base_score = self.__calculate_base_score()
+    self.temporal_score = self.__calculate_temporal_score()
+    self.environmental_score = self.__calculate_environmental_score()
+    self.vector_string = self.get_vector_string()
+
 
   def set_temporal_metrics(self, temporal_metrics):
     temporal_metrics = {k.lower(): v for k, v in temporal_metrics.items()}
@@ -60,6 +61,11 @@ class CVSS():
       raise KeyError('Not all temporal metrics were set.')
     
     self.temporal_score = self.__calculate_temporal_score()
+
+    self.environmental_score = self.__calculate_environmental_score
+
+    self.vector_string = self.get_vector_string()
+
     return self.temporal_score
   
 
@@ -82,7 +88,56 @@ class CVSS():
       raise KeyError('Not all evnironmental metrics were set.')
     
     self.environmental_score = self.__calculate_environmental_score()
+    self.vector_string = self.get_vector_string()
     return self.environmental_score
+
+
+  def print_scores(self):
+    print("Base Score: {base}".format(base=str(self.base_score)))
+    print("Temporal Score: {temporal}".format(temporal=str(self.temporal_score)))
+    print("Environmental Score: {environmental}".format(environmental=str(self.environmental_score)))
+    print('')
+    print('Vector String: {vs}'.format(vs=self.vector_string))
+    self.get_vector_string()
+
+
+  def get_vector_string(self):
+    vs = "CVSS:3.1/"
+
+    # Required variables
+
+    vs += "AV:{av}/AC:{ac}/PR:{pr}/UI:{ui}/S:{s}/C:{c}/I:{i}/A:{a}".format(av=self.av.upper(), ac=self.ac.upper(), pr=self.pr.upper(), ui=self.ui.upper(), s=self.s.upper(), c=self.c.upper(), i=self.i.upper(), a=self.a.upper())
+
+    if self.e.lower() != "x":
+      vs += '/E:{e}'.format(e=self.e.upper())
+    if self.rl.lower() != "x":
+      vs += '/RL:{rl}'.format(rl=self.rl.upper())
+    if self.rc.lower() != "x":
+      vs += '/RC:{rc}'.format(rc=self.rc.upper())
+    if self.cr.lower() != "x":
+      vs += '/CR:{cr}'.format(cr=self.cr.upper())
+    if self.ir.lower() != "x":
+      vs += '/IR:{ir}'.format(ir=self.ir.upper())
+    if self.ar.lower() != "x":
+      vs += '/AR:{ar}'.format(ar=self.ar.upper())
+    if self.mav.lower() != "x":
+      vs += '/MAV:{mav}'.format(mav=self.mav.upper())
+    if self.mac.lower() != "x":
+      vs += '/MAC:{mac}'.format(mac=self.mac.upper())
+    if self.mpr.lower() != "x":
+      vs += '/MPR:{mpr}'.format(mpr=self.mpr.upper())
+    if self.mui.lower() != "x":
+      vs += '/MUI:{mui}'.format(mui=self.mui.upper())
+    if self.ms.lower() != "x":
+      vs += '/MS:{ms}'.format(ms=self.ms.upper())
+    if self.mc.lower() != "x":
+      vs += '/MC:{mc}'.format(mc=self.mc.upper())
+    if self.mi.lower() != "x":
+      vs += '/MI:{mi}'.format(mi=self.mi.upper())
+    if self.ma.lower() != "x":
+      vs += '/MA:{ma}'.format(ma=self.ma.upper())
+
+    return vs
 
 
   def __calculate_base_score(self):
@@ -100,20 +155,20 @@ class CVSS():
       impact = 6.42 * iss
       pr = self.__assign_numerical_values('pr', self.pr)
 
-    explotability = 8.22 * self.__assign_numerical_values('av', self.av) * self.__assign_numerical_values('ac', self.ac) * pr * self.__assign_numerical_values('ui', self.ui)
+    exploitability = 8.22 * self.__assign_numerical_values('av', self.av) * self.__assign_numerical_values('ac', self.ac) * pr * self.__assign_numerical_values('ui', self.ui)
 
     if impact <= 0:
       base_score = 0
     elif self.s == 'c':
-      base_score = roundup(min([1.08 * (impact + explotability), 10]))
+      base_score = roundup(min([1.08 * (impact + exploitability), 10]))
     else:
-      base_score = roundup(min([(impact + explotability), 10]))
+      base_score = roundup(min([(impact + exploitability), 10]))
     
     return base_score
 
 
   def __calculate_temporal_score(self):
-    return roundup(self.base_score * self.e * self.rl * self.rc)
+    return roundup(self.base_score * self.__assign_numerical_values('e', self.e) * self.__assign_numerical_values('rl', self.rl) * self.__assign_numerical_values('rc', self.rc))
 
 
   def __calculate_environmental_score(self):
@@ -133,7 +188,26 @@ class CVSS():
       impact = 6.42 * miss
       mpr = self.__assign_numerical_values('mpr', self.mpr)
 
-    print(str(miss))
+    exploitability = 8.22 * self.__assign_numerical_values('mav', self.mav) * self.__assign_numerical_values('mac', self.mac) * mpr * self.__assign_numerical_values('mui', self.mui)
+
+    if impact <= 0:
+      env_score = 0
+    elif self.ms == 'c':
+      env_score = roundup(
+        roundup(min(1.08 * (impact + exploitability), 10))*
+        self.__assign_numerical_values('e', self.e) *
+        self.__assign_numerical_values('rl', self.rl) * 
+        self.__assign_numerical_values('rc', self.rc)
+      )
+    else:
+      env_score = roundup(
+        roundup(min((impact + exploitability), 10))*
+        self.__assign_numerical_values('e', self.e) *
+        self.__assign_numerical_values('rl', self.rl) * 
+        self.__assign_numerical_values('rc', self.rc)
+      )
+    
+    return env_score
 
 
   def __assign_numerical_values(self, type, value, modifier=False):
@@ -175,6 +249,8 @@ class CVSS():
       'H': 0.5,
     }
 
+    return_value = None
+
     if type.upper() in base_metrics:
       if value.upper() in base_metrics[type.upper()]:
         return_value = base_metrics[type.upper()][value.upper()]
@@ -187,6 +263,8 @@ class CVSS():
       if value.upper() in environmental_metrics[type.upper()]:
         return_value = environmental_metrics[type.upper()][value.upper()]
 
+    # 'PR' (Privileges Required) values L and H get modified if the 'S' (scope) is changed.
+    # if 'S' == 'C', PR: L = 0.68 and PR: H = 0.5
     if modifier and type.upper() == 'PR':
       return_value = modified_pr[value.upper()]
     elif modifier and type.upper() == 'MPR':
@@ -196,30 +274,30 @@ class CVSS():
 
 
 def roundup(floating_number):
-  print(floating_number)
+  # print(str(floating_number))
   if type(floating_number) == int:
     return floating_number
   elif type(floating_number) == str or type(floating_number) == list:
     raise ValueError("How did you do this.")
   else:
     split_number = str(floating_number).split('.')
-
-    if (split_number[1][1:]) != 0:
+    # print(str(split_number[1][1:]))
+    if (split_number[1][1:]) != 0 and (split_number[1][1:]):
       split_number[1] = split_number[1][:1] + '9'
       floating_number = float('.'.join(split_number))
-      
+    # print(str(round(floating_number, 1)))
     return round(floating_number, 1)
 
 
 base_metrics = {
-  "AV": "A",
+  "AV": "N",
   "AC": "L",
   "PR": "N",
-  "UI": "N",
-  "S": "U",
-  "C": "H",
+  "UI": "R",
+  "S": "C",
+  "C": "N",
   "I": "H",
-  "A": "H",
+  "A": "N",
 }
 
 temporal_metrics = {
@@ -243,8 +321,9 @@ environmental_metrics = {
 }
 
 
-
+'''
 cvss = CVSS(metrics=base_metrics)
 cvss.set_environmental_metrics(environmental_metrics)
 
-print(cvss.base_score)
+cvss.print_scores()
+'''
